9fcfd6def10860f7501f9f4a581901ae2b4464f6
aribilgiozgur <aribilgi.ozgur@yandex.com>
2014-10-26 11:32:38 +0200

    Initial Commit

---
+++ MVC01/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using MVC01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC01.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Contact() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Contact(ContactUser user) {
+            /*String name = formCol.Get("firstName");
+            String surname = formCol.Get("lastName");
+            String email = formCol.Get("eMail");
+            DateTime dateOfBirth = DateTime.Parse(formCol.Get("dateOfBirth"));
+            */
+            /*Insert işlemi...*/
+
+            return View("Index");
+        }
+
+        
+
+    }
+}
---
+++ MVC01/Models/ContactUser.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC01.Models
+{
+    public class ContactUser
+    {
+        public String firstName {get; set;}
+        public String lastName { get; set; }
+        public String eMail { get; set; }
+        public DateTime dateOfBirth { get; set; }
+    }
+}
